3e8f6306844f328a318b5202ec19c3bb70ef2b3c
Abhinavv1524 <choudharyabhinav818@gmail.com>
2024-09-14 20:01:16 +0530

    first-react-app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NewButton from "./NewButton";
+
+function App(){
+  return <div>
+    <h1>
+      Hello, World
+    </h1>
+    <NewButton></NewButton>
+  </div> 
+}
+
+export default App;
---
+++ src/NewButton.jsx
@@ -0,0 +1,5 @@
+function NewButton(){
+    return <button>Hey everyone</button>
+}
+
+export default NewButton;
